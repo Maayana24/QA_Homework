493e3c4f13a193f8c8ae2e0738b1ba9a62463882
TheAmazingGssi <105499786+TheAmazingGssi@users.noreply.github.com>
2026-06-04 16:08:46 +0300

    me commit

---
+++ Assets/Scripts/Torch.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Torch : MonoBehaviour
+{
+    [SerializeField] GameObject lightSource;
+    [SerializeField] MeshRenderer torchHead;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
